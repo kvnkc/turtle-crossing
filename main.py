5419fce8522eabcbba1707f659c2792abcd51874
kevin cheung <kevin.cheung.kai@gmail.com>
2022-10-20 00:12:19 -0400

    Added car class and collision function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from player import Player
+from car_manager import CarManager
 import time
 
 screen = Screen()
@@ -8,6 +9,8 @@ screen.title('Turtle Crossing')
 screen.tracer(0)
 
 player = Player()
+car_manager = CarManager()
+
 screen.listen()
 screen.onkey(player.up, 'Up')
 
@@ -16,5 +19,11 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
+    car_manager.generate_car()
+    car_manager.move_car()
+
+    # Detect Player collision
+    if player.distance(car_manager) < 30:
+        print('game over')
 
 screen.exitonclick()
