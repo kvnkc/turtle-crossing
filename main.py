a15660ad5f84c69fe68252e954afb3eb88db97c7
kevin cheung <kevin.cheung.kai@gmail.com>
2022-10-19 22:52:43 -0400

    created player class and added movement function

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-from ast import While
 from turtle import Screen
+from player import Player
 import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
-screen.exitonclick()
 screen.title('Turtle Crossing')
-screen.listen()
 screen.tracer(0)
+
+player = Player()
+screen.listen()
+screen.onkey(player.up, 'Up')
 
 game_is_on = True
 
 while game_is_on:
     screen.update()
     time.sleep(0.1)
+
+screen.exitonclick()
